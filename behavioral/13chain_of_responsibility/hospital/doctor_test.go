3248eaa305f9f4fc7c807ad6e1cdb49b90161f50
aaronlyc <blockchain_lyc@163.com>
2020-11-13 00:44:59 +0800

    add ginkgo test case

--- behavioral/13chain_of_responsibility/hospital/doctor_test.go
+++ behavioral/13chain_of_responsibility/hospital/doctor_test.go
@@ -1,17 +1,15 @@
 package hospital_test
 
 import (
+	. "design_patterns/behavioral/13chain_of_responsibility/hospital"
 	. "github.com/onsi/ginkgo"
-	//. "github.com/onsi/gomega"
-
-	"design_patterns/behavioral/13chain_of_responsibility/hospital"
 )
 
 var _ = Describe("Doctor", func() {
-	cashier := &hospital.Cashier{}
-	medical := &hospital.Medical{}
-	doctor := &hospital.Doctor{}
-	reception := &hospital.Reception{}
+	cashier := &Cashier{}
+	medical := &Medical{}
+	doctor := &Doctor{}
+	reception := &Reception{}
 
 
 	BeforeEach(func() {
@@ -20,17 +18,18 @@ var _ = Describe("Doctor", func() {
 		reception.SetNext(doctor)
 	})
 
+
 	Describe("测试病人不同阶段的流程", func() {
 		Context("病人刚开始来的流程", func() {
 			It("这将从前台开始", func() {
-				patient := &hospital.Patient{Name: "start man"}
+				patient := &Patient{Name: "start man"}
 				reception.Execute(patient)
 			})
 		})
 
 		Context("病人已完成医生流程", func() {
 			It("这将显示", func() {
-				patient := &hospital.Patient{Name: "docker looked", DoctorCheckUpDone: true, RegistrationDone: true}
+				patient := &Patient{Name: "docker looked", DoctorCheckUpDone: true, RegistrationDone: true}
 				reception.Execute(patient)
 			})
 		})
