3fe17d245a666d37821559c3723f98783d3650c1
aaronlyc <blockchain_lyc@163.com>
2020-11-20 17:38:34 +0800

    add adapter builder and prototype pattern

---
+++ creational/5builder/iBuilder/builder.go
@@ -0,0 +1,18 @@
+package iBuilder
+
+type iBuilder interface {
+	setWindowType()
+	setDoorType()
+	setNumFloor()
+	getHouse() house
+}
+
+func getBuilder(builderType string) iBuilder {
+	switch builderType {
+	case "normal":
+		return &normalBuilder{}
+	case "igloo":
+		return &iglooBuilder{}
+	}
+	return nil
+}
